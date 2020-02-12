0f0d02c5533f051df5ba86654a0aa53247db9a8a
Vikas2004 <S538336@NWMISSOURI.EDU>
2020-02-12 10:20:47 -0600

    color change

--- main.js
+++ main.js
@@ -3,9 +3,8 @@ const updateColor = () => {
   const r = parseInt(document.querySelector('#red').value)
   const g = parseInt(document.querySelector('#green').value)
   const b = parseInt(document.querySelector('#blue').value)
-  const v = parseInt(document.querySelector('#violet').value)
   const canvasElement = document.querySelector('#canvas')
-  canvasElement.style.backgroundColor = `rgbv(${r},${g},${b},${v})`
+  canvasElement.style.backgroundColor = `rgbv(${r},${g},${b})`
   canvasElement.style.border = "thin solid black"
 }
 
@@ -13,11 +12,9 @@ const updateColor = () => {
 document.querySelector('#red').addEventListener('click', updateColor)
 document.querySelector('#blue').addEventListener('click', updateColor)
 document.querySelector('#green').addEventListener('click', updateColor)
-document.querySelector('#violet').addEventListener('click', updateColor)
 document.querySelector('#red').addEventListener('touchend', updateColor)
 document.querySelector('#blue').addEventListener('touchend', updateColor)
 document.querySelector('#green').addEventListener('touchend', updateColor)
-document.querySelector('#violet').addEventListener('touchend', updateColor)
 
 // call the handler 
 updateColor() 
