3a157f4f72bf2d46426fe05bd459f6ed04c81261
Vikas2004 <S538336@NWMISSOURI.EDU>
2020-02-12 10:15:10 -0600

    edited a click event

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const updateColor = () => {
   const r = parseInt(document.querySelector('#red').value)
   const g = parseInt(document.querySelector('#green').value)
   const b = parseInt(document.querySelector('#blue').value)
+  const v = parseInt(document.querySelector('#violet').value)
   const canvasElement = document.querySelector('#canvas')
   canvasElement.style.backgroundColor = `rgb(${r},${g},${b})`
   canvasElement.style.border = "thin solid black"
@@ -12,6 +13,7 @@ const updateColor = () => {
 document.querySelector('#red').addEventListener('click', updateColor)
 document.querySelector('#blue').addEventListener('click', updateColor)
 document.querySelector('#green').addEventListener('click', updateColor)
+document.querySelector('#violet').addEventListener('click', updateColor)
 document.querySelector('#red').addEventListener('touchend', updateColor)
 document.querySelector('#blue').addEventListener('touchend', updateColor)
 document.querySelector('#green').addEventListener('touchend', updateColor)
